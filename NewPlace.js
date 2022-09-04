08445d2f40a4f969c7b881cd3a7335e06303eb10
BarakOren <barakoren5@gmail.com>
2022-09-05 01:21:46 +0300

    custom form

--- NewPlace.js
+++ NewPlace.js
@@ -1,8 +1,12 @@
-import React, {useReducer, useCallback} from 'react';
-import "./NewPlace.css";
-import Input  from '../../shared/components/FormElements/Input';
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../util/validators"
-import Button from "../../shared/components/FormElements/Button"
+import React, { useCallback, useReducer } from 'react';
+
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import './NewPlace.css';
 
 const formReducer = (state, action) => {
   switch (action.type) {
@@ -52,27 +56,30 @@ const NewPlace = () => {
     });
   }, []);
 
-  return <form className="place-form">
-    <Input 
-     id="title"
-     element="input"
-     type="text" 
-     label="Title" 
-     validators={[VALIDATOR_REQUIRE()]}
-     errorText="please enter a valid text"
-     onInput={inputHandler}
-     />
-     <Input 
-     id="description"
-     element="textarea"
-     type="text" 
-     label="Title" 
-     validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
-     errorText="please enter a valid description (at least 5 characters)."
-     onInput={inputHandler}
-     />
-     <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
-  </form>
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Button type="submit">
+        ADD PLACE
+      </Button>
+    </form>
+  );
 };
 
 export default NewPlace;
